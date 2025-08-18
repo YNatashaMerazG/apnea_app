607ed2ea5452f40041916ed4092a834566737535
Natasha meraz <natashamerazg@gmail.com>
2025-08-18 11:57:09 -0600

    Cambio de alertas a espanol

--- apnea/settings.py
+++ apnea/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -116,6 +116,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+TIME_ZONE = 'America/Mexico_City'
+
 
 
 # Static files (CSS, JavaScript, Images)
