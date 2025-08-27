19b964476ce9d64c6d8906242e04d5682536de01
Natasha meraz <natashamerazg@gmail.com>
2025-08-27 11:17:47 -0600

    Correccion de doble mensaje en recuperar contrasena v3

--- apneasueno/views.py
+++ apneasueno/views.py
@@ -294,6 +294,7 @@ def restablecer_contrasena(request):
             user = User.objects.get(username=username)
             user.set_password(nueva_contrasena)
             user.save()
+            messages.success(request, 'Contraseña actualizada correctamente.')
             return redirect('doctor_login')
         else:
             messages.error(request, 'Corrige los errores del formulario.')
